2c8fdcfe8f2726254e6d2a90c843bdd68eb5c793
fetma <fetma@ZOMBATKA>
2025-06-16 01:34:44 +0300

    Добавьте файлы проекта.

---
+++ LabWork41/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabWork41
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public bool IsMenuOpened = false;
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsMenuOpened = !IsMenuOpened;
+            MenuDoubleAnimation.From = IsMenuOpened ? 20 : 100;
+            MenuDoubleAnimation.To = IsMenuOpened ? 100 : 20;
+        }
+    }
+}
